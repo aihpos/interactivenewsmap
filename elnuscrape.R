13b03ddd8eaa7896b9fd28071e487d6b9eb52b09
“Dana <eunseochoi2019@u.northwestern.edu>
2017-07-19 17:04:06 +0200

    el nuevo dia scrape

---
+++ elnuscrape.R
@@ -0,0 +1,11 @@
+url <- https://www.elnuevodia.com/english/#
+  
+  webpage <- read_html(url)
+
+headline_data_html <- html_nodes(webpage, ".story-tease-title a , #Leaderboard iframe")
+
+headline_data <- html_text(headline_data_html)
+
+headlines_ELNU <- as.data.frame(headline_data)
+
+View(headlines_ELNU)
