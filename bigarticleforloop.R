fa44c6003f93ffa1eb0171a91b047c1c2c18bfab
Tristan Prinz <tprinz96@g.ucla.edu>
2017-07-21 00:01:31 +0200

    data frame for megan

--- bigarticleforloop.R
+++ bigarticleforloop.R
@@ -24,5 +24,16 @@ for(x in 1:nrow(news)){
   bigdfarticles <- rbind(bigdfarticles, articles2_new)
 }
 
+news[x,1] -> news_id
+newsurl = sprintf("https://newsapi.org/v1/articles?source=%s&apiKey=%s", news_id, apikey )
+articles_new <- GET(newsurl)
+articles1_new <- fromJSON(content(articles_new, as = "text")) 
 
+articles1_new <- as.data.frame(articles1_new)
+articles1_new$source <- as.character(articles1_new$source)
+articles1_new$articles.title <- as.character(articles1_new$articles.title)
+articles1_new$articles.url <- as.character(articles1_new$articles.url)
+
+articles2_new <- articles1_new %>% subset(select = c("source", "articles.title", "articles.url"))
+bigdfarticles <- rbind(bigdfarticles, articles2_new)
 
