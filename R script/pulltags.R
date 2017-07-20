f3454267f104fa5bba1512933f552dfa22b85b45
Sophia Dong <sophia.dong@rutgers.edu>
2017-07-20 10:17:32 +0200

    function to take text and return tags

---
+++ R script/pulltags.R
@@ -0,0 +1,44 @@
+# function to take string and pull location tags from it
+
+apikey_textrazor <- "f9a1e365d6ed12cf4ac71a5c0c568c10fe10b581aa4ad2035c3f9602"
+
+baseurl <- "http://api.textrazor.com"
+
+newurl <- "https://www.nytimes.com/interactive/2017/07/19/climate/two-weeks-mcmurdo-antarctica.htm"
+
+text <- "text=Two Weeks on Ice in McMurdo Station, Antarctica"
+
+key <- "MY API KEY"
+text <- "text=Spain's stricken Bankia expects to sell off its vast portfolio of industrial holdings that includes a stake in the parent company of British Airways and Iberia"
+
+extractors <- "&extractors=entities"
+
+doc <- POST("https://api.textrazor.com",
+            add_headers("x-textrazor-key" = apikey_textrazor),
+            body = paste0(text, extractors)
+)
+doc
+
+doc <- fromJSON(content(doc, as = "text"))
+
+# want to pull one specific location from this list
+
+library(purrr)
+library(tidyr)
+
+doc2 <- doc
+
+# gets only place entities
+doc2$response$entities$type <- map(doc$response$entities$type, 1)
+
+locationtags <- keep(doc2, doc2$response$entities$type == "Place")
+
+
+locationtags <- dplyr::filter(doc2, response$entities$type == "Place")
+
+locationtags <- filter(doc2, doc2$response$entities$type == "Place")
+
+locationtags <- subset(doc2, doc2$response$entities$type == "Place")
+
+#
+entitytype <- map(doc$response$entities$type, 1)
