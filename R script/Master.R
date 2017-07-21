0e33a7132fc0bdd7dc7a7fac19a3720e1f06ff56
Tristan Prinz <tprinz96@g.ucla.edu>
2017-07-21 08:24:42 +0200

    Final master

--- R script/Master.R
+++ R script/Master.R
@@ -1,5 +1,8 @@
 #Necessary packages
-
+library(shiny)
+library(sp)
+library(magrittr)
+library(leaflet)
 library(httr)
 library(jsonlite)
 library(lubridate)
@@ -115,4 +118,59 @@ for (x in 1:nrow(bigdfarticles3)){
 # all the location data with latitude + longitude stored in latlongframe
 
 #get rid of duplicate location entries in latlongframe
+
 latlongframe <- unique(latlongframe)
+
+#Shiny app with leaflet using data from above
+
+bigdfarticles2 <- bigdfarticles2 %>% mutate(location = locationtag)
+
+a <- bigdfarticles2 %>% left_join(latlongframe)
+
+factpal<- colorFactor(topo.colors(30), a$location)
+
+ui <- bootstrapPage(
+  tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
+  leafletOutput("map", width = "100%", height = "100%"),
+  absolutePanel(top = 10, right = 10,
+                textInput("Location", label = h3("Location"), value = "Enter text..."),
+                selectInput(inputId = "source", label = h3("Select Sources"),
+                            choices = c("Time" = 1, "The Washington Post" = 2, "The Times of India" = 3,
+                                        "The Telegraph" = 4, "The New York Times" = 5, "The Huffington Post" = 6,
+                                        "Hindustan times" = 7, "The Guardian" = 8, "Reuters" = 9,
+                                        "Independent" = 10, "BBC News" = 11, "CNN" = 12, "Al-jazeera" = 13,
+                                        "Associated-press" = 14, "Metro" = 15),
+                            selected = 1)
+  )
+)
+
+
+server <- function(input, output, session) {
+  
+  output$map <- renderLeaflet({
+    leaflet(a) %>% 
+      addTiles() %>%
+      addCircleMarkers(
+        data = a, lng = ~long, lat = ~lat, popup = ~(articles.title),
+        color = ~factpal(location),
+        stroke = FALSE, fillOpacity = 0.5,
+        radius = runif(100, 4, 10)  )
+  })
+  
+  observe({
+    news <- input$source
+    loc<- input$location
+    site <- a %>%
+      filter(location %in% loc, source %in% news)
+    
+    leafletProxy("map") %>%
+      addCircleMarkers(
+        data = a, lng = ~long, lat = ~lat, popup = ~(articles.title),
+        color = ~factpal(location),
+        stroke = FALSE, fillOpacity = 0.5,
+        radius = runif(100, 4, 10)  )
+  })
+  
+}
+
+shinyApp(ui, server) 
