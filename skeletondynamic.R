c696bc960aab1e4b2fe1dc24edc0d0f6c9eaa9c4
“Dana <eunseochoi2019@u.northwestern.edu>
2017-07-21 03:45:17 +0200

    skeleton dynamic edit

--- skeletondynamic.R
+++ skeletondynamic.R
@@ -2,6 +2,7 @@ library(dplyr)
 library(leaflet)
 library(sp)
 library(magrittr)
+library(dplyr)
 bigdfarticles2 <- bigdfarticles2 %>% mutate(location = locationtag)
 a <- left_join(bigdfarticles2, latlongframe, by = location)
 
@@ -15,29 +16,46 @@ ui <- bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   leafletOutput("map", width = "100%", height = "100%"),
   absolutePanel(top = 10, right = 10,
-                textInput("locationtag", label = h3("Landmark"), value = "Enter text..."),
-                textInput("City", label = h3("City"), value = "Enter text..."),
-                textInput("Country", label = h3("Country"), value = "Enter text..."),
-                textInput("source", label = h3("Source"), value = "Enter text...") 
+                textInput("Location", label = h3("Location"), value = "Enter text..."),
+                selectInput(inputId = "source", label = h3("Select Sources"),
+                            choices = c("Time" = 1, "The Washington Post" = 2, "The Times of India" = 3,
+                                        "The Telegraph" = 4, "The New York Times" = 5, "The Huffington Post" = 6,
+                                        "Hindustan times" = 7, "The Guardian" = 8, "Reuters" = 9,
+                                        "Independent" = 10, "BBC News" = 11, "CNN" = 12, "Al-jazeera" = 13,
+                                        "Associated-press" = 14, "Metro" = 15),
+                            selected = 1).
+                leafletOutput("map")
+                
   )
 )
 
 
 server <- function(input, output, session) {
-d <- leaflet() %>% addTiles() %>%
-  #  addMarkers(data = a, lng = ~long, lat = ~lat, popup = ~(location)) %>%
-  addCircleMarkers(
-    data = a, lng = ~long, lat = ~lat, popup = ~(articles.title),
-    color = ~factpal(location),
-    stroke = FALSE, fillOpacity = 0.5,
-    radius = runif(100, 4, 10)
-  )
-
-output$map <- renderLeaflet({
-  leaflet() %>% 
-  addTiles() %>% 
-  addMarkers(data = a, lng = ~long, lat = ~lat, popup = ~(location))
+  
+  output$map <- renderLeaflet({
+    leaflet(a) %>% 
+      addTiles() %>%
+      addCircleMarkers(
+        data = a, lng = ~long, lat = ~lat, popup = ~(articles.title),
+        color = ~factpal(location),
+        stroke = FALSE, fillOpacity = 0.5,
+        radius = runif(100, 4, 10)  )
   })
+  
+  observe({
+    news <- input$source
+    loc<- input$location
+    a <- a %>%
+      filter(a$location %in% loc, a$source %in% news)
+    
+    leafletProxy("map") %>% clearMarkers %>%
+      addCircleMarkers(
+        data = a, lng = ~long, lat = ~lat, popup = ~(articles.title),
+        color = ~factpal(location),
+        stroke = FALSE, fillOpacity = 0.5,
+        radius = runif(100, 4, 10)  )
+  })
+  
 }
 
-shinyApp(ui, server)
+shinyApp(ui, server) 
